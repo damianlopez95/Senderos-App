bdd03d4c80972b274f0ff9a796b341833da461d3
Damian <damian.lopez.041995@gmail.com>
2019-10-15 15:28:04 -0300

    Añadida SideBar

--- App/Containers/FirstScreen.js
+++ App/Containers/FirstScreen.js
@@ -5,27 +5,27 @@ import styles from './Styles/FirstScreenStyles'
 
 export default class FirstScreen extends Component {
 
-    render () {
-      return (
-        <View style={styles.container}>
-          <ScrollView>
-            <Text style={styles.titulo}>
-              SenderosApp
-            </Text>
-            <View style={styles.image_container}>
-                <Image
-                  style={styles.image}
-                  source={{uri: 'https://gremialesdelsur.com.ar/data/img_cont/img_imagenes/4952.jpg'}}
-                />
-            </View>
-          </ScrollView>
-          <View style={styles.btnSenderos}>
-            <Button 
-              title="Ver senderos"
-              onPress={() => this.props.navigation.navigate('MapScreen')}
-            />
+  render () {
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.titulo}>
+            SenderosApp
+          </Text>
+          <View style={styles.image_container}>
+              <Image
+                style={styles.image}
+                source={{uri: 'https://gremialesdelsur.com.ar/data/img_cont/img_imagenes/4952.jpg'}}
+              />
           </View>
+        </ScrollView>
+        <View style={styles.btnSenderos}>
+          <Button 
+            title="Ver senderos"
+            onPress={() => this.props.navigation.navigate('MapScreen')}
+          />
         </View>
-      )
-    }
+      </View>
+    )
+  }
 }
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,24 +1,79 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
-//import LaunchScreen from '../Containers/LaunchScreen'
+import React, { Component } from 'react'
+import { 
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+   } from 'react-native'
+import { createStackNavigator, createDrawerNavigator, createAppContainer } from 'react-navigation'
+
 import FirstScreen from '../Containers/FirstScreen'
 import MapScreen from '../Containers/MapScreen'
 
 import styles from './Styles/NavigationStyles'
+import SideMenu from './SideMenu'
 
-// Manifest of possible screens
-const PrimaryNav = createStackNavigator({
-  
-  //Pantalla inicial de la app
-  FirstScreen: { screen: FirstScreen },
-  MapScreen: { screen: MapScreen }
-  //LaunchScreen: { screen: LaunchScreen }
-}, {
-  // Default config for all screens
-  headerMode: 'none',
-  initialRouteName: 'FirstScreen',
-  navigationOptions: {
-    headerStyle: styles.header
+//Navigation Drawer Structure for all screen
+class NavigationDrawerStructure extends Component {
+  toggleDrawer = () => {
+    this.props.navigationProps.toggleDrawer();
+  };
+  render() {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
+          {/*Donute Button Image */}
+          <Text>Menu</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
-})
+}
 
-export default createAppContainer(PrimaryNav)
+const FirstActivity_StackNavigator = createStackNavigator({
+  //All the screen from the First Option will be indexed here
+  First: {
+    screen: FirstScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Inicio',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#2ECC40',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+ 
+//Stack Navigator for the Second Option of Navigation Drawer
+const Screen2_StackNavigator = createStackNavigator({
+  //All the screen from the Second Option will be indexed here
+  Second: {
+    screen: MapScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Mapa',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+ 
+      headerStyle: {
+        backgroundColor: '#2ECC40',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+
+//Drawer Navigator for the Navigation Drawer / Sidebar
+const Drawer = createDrawerNavigator(
+  {
+    //Drawer Optons and indexing
+    NavScreen1: { screen: FirstActivity_StackNavigator },
+    NavScreen2: { screen: Screen2_StackNavigator },
+  },
+  {
+    contentComponent: SideMenu,
+    drawerWidth: Dimensions.get('window').width - 120,
+  }
+);
+export default createAppContainer(Drawer);
+
