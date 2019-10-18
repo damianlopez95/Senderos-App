4666506be5a6acfdb6307ae49888962d8cd18d8e
Sergio Maciel <dario.maciel.91@gmail.com>
2019-10-18 18:44:04 -0300

    Fix Api Senderos

--- App/Services/SenderoApi.js
+++ App/Services/SenderoApi.js
@@ -1,6 +1,6 @@
 import apisauce from 'apisauce'
 
-const create = (baseURL = 'https://sib.gob.ar/api/2.0.0/senderos') => {
+const create = (baseURL = 'https://sib.gob.ar/api/2.0.0') => {
 
    const api = apisauce.create({
       baseURL,
@@ -9,42 +9,8 @@ const create = (baseURL = 'https://sib.gob.ar/api/2.0.0/senderos') => {
    })
 
 
-   // const getId = () => api.get('1')
-   // const getList = () => api.get('list')
-   // const search = (name) => api.get('search/sendero', {q: name})
-   const getId =  {
-      "id": 1,
-      "nombre":'Lon Andes',
-      "imagen": 'https://picsum.photos/300/200',
-      "descripcion": 'Descripcion de un sendero'
-   }
-
-   const getList =[
-      {
-         "id": 1,
-         "nombre":'Lon Andes',
-         "imagen": 'https://picsum.photos/300/200',
-         "descripcion": 'Descripcion de un sendero'
-      },
-      {
-         "id": 2,
-         "nombre":'Pico Alto',
-         "imagen": 'https://picsum.photos/300/200',
-         "descripcion": 'Descripcion de un sendero'
-      },
-      {
-         "id": 3,
-         "nombre":'Camino Verde',
-         "imagen": 'https://picsum.photos/300/200',
-         "descripcion": 'Descripcion de un sendero'
-      },
-      {
-         "id": 4,
-         "nombre":'Tembleque',
-         "imagen": 'https://picsum.photos/300/200',
-         "descripcion": 'Descripcion de un sendero'
-      },
-   ]
+   const getId = (id) => api.get(`/senderos/${id}`)
+   const getList = () => api.get(`/ficha-area-protegida/parque-nacional-los-glaciares/senderos`)
 
    return {
       getId,
